92f2c5b1427f88cfad37a4347aa8c547481a3387
David Simon <david.simon@color.com>
2023-11-16 17:10:25 -0500

    Better timeline scaling

--- src/components/dashboard/timeline.tsx
+++ src/components/dashboard/timeline.tsx
@@ -6,15 +6,19 @@ import {
   useSignal,
   useVisibleTask$,
 } from "@builder.io/qwik";
-import { Timeline as VisTimeline } from "vis-timeline";
 
+import { Timeline as VisTimeline } from "vis-timeline";
 import "vis-timeline/dist/vis-timeline-graph2d.min.css";
+import moment from "moment";
 
 import { HourlyWeatherForecast } from "../data/types";
 
 interface TimelineProps {
   forecasts: HourlyWeatherForecast[];
 }
+
+const VISIBLE_TIME_SPAN_HOURS = 12;
+const ROLLING_OFFSET = 0.1;
 
 export const Timeline = component$<TimelineProps>((props) => {
   const visTimeline = useSignal<NoSerialize<VisTimeline>>();
@@ -32,11 +36,28 @@ export const Timeline = component$<TimelineProps>((props) => {
     if (visTimeline.value !== undefined) {
       visTimeline.value.destroy();
     }
+
+    const now = moment();
+    const start = moment(now).subtract(
+      VISIBLE_TIME_SPAN_HOURS * ROLLING_OFFSET,
+      "hours"
+    );
+    const end = moment(now).add(
+      VISIBLE_TIME_SPAN_HOURS * (1.0 - ROLLING_OFFSET),
+      "hours"
+    );
     visTimeline.value = noSerialize(
       new VisTimeline(document.getElementById(id)!, items, {
-        rollingMode: { follow: true, offset: 0.1 },
-        height: "15rem",
+        rollingMode: { follow: true, offset: ROLLING_OFFSET },
+        height: "10rem",
         selectable: false,
+        zoomable: false,
+        start: start.toDate(),
+        end: end.toDate(),
+        timeAxis: { scale: "hour", step: 1 },
+        format: {
+          minorLabels: (date) => moment(date).format("ha"),
+        },
       })
     );
   });
